de261be5b9588b57bc7f4605640e8ae9bbee2d17
Josue Cruz <jecruzz@puce.edu.ec>
2025-08-01 01:45:16 -0500

    creacion de paginas, faltan base de datos

--- PlanNet/settings.py
+++ PlanNet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'PlanNet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'hangouts', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'PlanNet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'plannet_db',
+        'USER': 'postgres',
+        'PASSWORD': '013013',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -116,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "hangouts","static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- hangouts/views.py
+++ hangouts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+
+def dashboard(request):
+    return render(request, "dashboard.html")
+
+
+def events(request):
+    return render(request, "events.html")
+
+
+def create_event(request):
+    return render(request, "create_event.html")
+
+
+def profile(request):
+    return render(request, "profile.html")
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
